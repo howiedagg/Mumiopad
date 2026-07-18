b802e1d8b58dbb37078c0359025613dabaa661dd
howiedagg <codehowie@gmail.com>
2026-07-18 11:04:40 +0800

    滾動2

--- android-app/app/src/main/java/com/example/vrtouchpad/ui/components/Touchpad.kt
+++ android-app/app/src/main/java/com/example/vrtouchpad/ui/components/Touchpad.kt
@@ -74,7 +74,7 @@ fun Touchpad(
                     LocalFeedbackType.TICK -> {
                         // 【恢復】：採用您覺得力度剛好的上一版震動定義，回復扎實的力道
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                            HapticFeedbackConstants.CONFIRM
+                            HapticFeedbackConstants.SEGMENT_FREQUENT_TICK
                         } else {
                             HapticFeedbackConstants.VIRTUAL_KEY
                         }
